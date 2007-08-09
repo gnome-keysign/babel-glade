def98ae6fcda1b41cb3fbf68abfb2c04b6d8a200
Christopher Lenz <cmlenz@gmail.com>
2007-08-09 15:58:10 +0000

    Renamed directory of Glade extractor plugin.
    git-svn-id: https://svn.edgewall.org/repos/babel/contrib/glade@257 59ecc08e-a131-0410-9ea7-d4c0f28ac310

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: sw=4 ts=4 fenc=utf-8
+# =============================================================================
+# $Id$
+# =============================================================================
+#             $URL$
+# $LastChangedDate$
+#             $Rev$
+#   $LastChangedBy$
+# =============================================================================
+# Copyright (C) 2006 Ufsoft.org - Pedro Algarvio <ufs@ufsoft.org>
+#
+# Please view LICENSE for additional licensing information.
+# =============================================================================
+
+from setuptools import setup
+
+setup(
+    name    = 'BabelGladeExtractor',
+    version = '0.2',
+    license = 'BSD',
+    author  = 'Pedro Algarvio',
+    author_email = 'ufs@ufsoft.org',
+    maintainer = 'Pedro Algarvio',
+    maintainer_email = 'ufs@ufsoft.org',
+    description = 'Babel Glade XML files translatable strings extractor',
+    url = 'http://babel.edgewall.org/wiki/BabelGladeExtractor',
+    keywords = ['PyGTK', 'Glade', 'gettext', 'Babel', 'I18n', 'L10n'],
+    install_requires = ['Babel'],
+    entry_points = """
+    [babel.extractors]
+    glade = babelglade:extract_glade
+    """,
+    packages = ['babelglade']
+
+)
