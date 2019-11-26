90f6f026762ff41694c69e8ef5fcfda28dbc6a4e
Arjan Molenaar <gaphor@gmail.com>
2019-11-26 23:19:48 +0100

    Add unit tests for translate module

---
+++ babelglade/tests/test_translate.py
@@ -0,0 +1,39 @@
+
+import os
+from babelglade.translate import translate_desktop_file, translate_appdata_file
+
+def relative(test_file):
+    return os.path.join(os.path.dirname(__file__), test_file)
+
+
+def test_desktop_file():
+    translate_desktop_file(relative("test.raw.desktop"), "test.desktop", relative("locale"))
+
+    with open("test.desktop") as desktop:
+        content = desktop.read()
+
+    assert "Comment=This should be translated." in content
+    assert "Comment[nl]=Dit moet worden vertaald." in content
+    assert "Comment[fr]=Cela devrait être traduit." in content
+
+def test_appdata_xml():
+    translate_appdata_file(relative("test.raw.appdata.xml"), "test.appdata.xml", relative("locale"))
+
+    with open("test.appdata.xml") as desktop:
+        content = desktop.read()
+
+    assert "<p>This should be translated.</p>" in content
+    assert '<p xml:lang="fr">Cela devrait être traduit.</p>' in content
+    assert '<p xml:lang="nl">Dit moet worden vertaald.</p>' in content
+    assert """<p>
+      Multi line
+      text.
+    </p>""" in content
+    assert """<p xml:lang="fr">
+      Texte
+      multi-ligne.
+    </p>""" in content
+    assert """<p xml:lang="nl">
+      Meerregelige
+      tekst.
+    </p>""" in content
